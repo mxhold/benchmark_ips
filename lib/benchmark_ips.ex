221b0f6fe640d19563f56fdae8dac4c669bce818
Max Holder <mxhold@gmail.com>
2015-09-04 15:10:40 -0700

    Improve naming and document functions

--- lib/benchmark_ips.ex
+++ lib/benchmark_ips.ex
@@ -14,10 +14,12 @@ defmodule BenchmarkIps do
   @doc """
   Calls the function the specified number of times
   """
-  def call_times(fun, 1), do: fun.()
   def call_times(fun, times) do
     fun.()
-    call_times(fun, times - 1)
+
+    if times > 1 do
+      call_times(fun, times - 1)
+    end
   end
 
   @doc """
@@ -30,50 +32,71 @@ defmodule BenchmarkIps do
     {result, finish - start}
   end
 
-  def cycles_per_100ms(iters, time_us) do
+  @doc """
+  Returns the number of iterations that should be able to be run in 100ms given a number
+  of iterations and a duration in microseconds
+  """
+  def iterations_per_100ms(iters, time_us) do
     time_ms = time_us * 1.0e-3
-    cycles_per_ms = iters / time_ms
-    cycles = cycles_per_ms * 100
-    cycles = round(cycles)
-    if cycles <= 0 do
+    iterations_per_ms = iters / time_ms
+    batches = iterations_per_ms * 100
+    batches = round(batches)
+    if batches <= 0 do
       1
     else
-      cycles
+      batches
     end
   end
 
-  def iterations_runnable_in_duration_by_cycles(fun, cycles, target_duration_us) do
-    iterations_runnable_in_duration_by_cycles(fun, cycles, now_us + target_duration_us, 0)
+  @doc """
+  Returns the number of iterations a function can run in the provided duration (microseconds) by
+  running it in batches and checking between batches on the time elapsed
+  """
+  def iterations_runnable_in_duration_by_batches(fun, batches, target_duration_us) do
+    iterations_runnable_in_duration_by_batches(fun, batches, now_us + target_duration_us, 0)
   end
 
-  def iterations_runnable_in_duration_by_cycles(fun, cycles, target_end_time_us, iters) do
+  def iterations_runnable_in_duration_by_batches(fun, batches, target_end_time_us, iters) do
     if now_us < target_end_time_us do
-      call_times(fun, cycles)
-      iterations_runnable_in_duration_by_cycles(fun, cycles, target_end_time_us, iters + 1)
+      call_times(fun, batches)
+      iterations_runnable_in_duration_by_batches(fun, batches, target_end_time_us, iters + 1)
     else
-      iters * cycles
+      iters * batches
     end
   end
 
-  def bench(fun, cycles, target_duration_s) do
+  @doc """
+  Returns the number of iterations and elapsed time of running a function in batches for the
+  provided target duration (seconds)
+  """
+  def bench(fun, batches, target_duration_s) do
     target_duration_us = target_duration_s * 1.0e6
 
     {iters, actual_time_us} = measure_in_us(fn ->
-      iterations_runnable_in_duration_by_cycles(fun, cycles, target_duration_us)
+      iterations_runnable_in_duration_by_batches(fun, batches, target_duration_us)
     end)
 
     {iters, actual_time_us}
   end
 
+  @doc """
+  Benchmarks a function and returns the iterations per second and microseconds per iterations
+
+  The function is first benchmarked for the provided warmup duration (seconds, default 1) in
+  batches of 1 in order to determine about how many iterations can run in 100ms.
+
+  It is then benchmarked in batches of that number of iterations for the provided bench duration
+  (seconds, default 2).
+  """
   def report(fun, options \\ []) do
     warmup_duration_s = Keyword.get(options, :warmup_duration_s, 1)
     bench_duration_s = Keyword.get(options, :bench_duration_s, 2)
 
     {warmup_iters, warmup_time_us} = bench(fun, 1, warmup_duration_s)
 
-    warmup_cycles_per_100ms = cycles_per_100ms(warmup_iters, warmup_time_us)
+    warmup_iterations_per_100ms = iterations_per_100ms(warmup_iters, warmup_time_us)
 
-    {actual_iters, actual_time_us} = bench(fun, warmup_cycles_per_100ms, bench_duration_s)
+    {actual_iters, actual_time_us} = bench(fun, warmup_iterations_per_100ms, bench_duration_s)
 
     iters_per_us = actual_iters / actual_time_us
 
